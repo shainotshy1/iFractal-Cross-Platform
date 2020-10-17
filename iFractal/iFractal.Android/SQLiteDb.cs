ab532c6b598f321eb856f1e3f5e105be8a850aef
Shai Dickman <shai.j.dickman@gmail.com>
2020-10-17 00:38:56 -0700

    Update All

---
+++ iFractal/iFractal.Android/SQLiteDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using iFractal.Droid;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
+namespace iFractal.Droid
+{
+	public class SQLiteDb : ISQLiteDb
+	{
+		public SQLiteAsyncConnection GetConnection()
+		{
+			var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			var path = Path.Combine(documentsPath, "iFractalSettings.db3");
+
+			return new SQLiteAsyncConnection(path);
+		}
+	}
+}
